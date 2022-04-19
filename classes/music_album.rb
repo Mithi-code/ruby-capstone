c2bb6316a0dfe6ff7c7005dac804026c182ed828
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-19 19:20:58 +0545

    Did change

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,16 +1,18 @@
-require '../item'
+require_relative '../item'
 
 class MusicAlbum < Item
   attr_accessor :on_spotify, :name, :publish_date
   attr_reader :items
 
-  def initialize(on_spotify, name, publish_date)
+  def initialize(id, publish_date, on_spotify, name)
     super(publish_date)
     @id = id
     @on_spotify = on_spotify
     @name = name
     @items = []
   end
+
+  private
 
   def can_be_archived?
     super && @on_spotify
