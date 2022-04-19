fea8b70ed4f71d8be7fe5252a9965e694652ce68
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-19 15:49:09 +0545

    Implement add_item method in the Genre class

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,3 +1,17 @@
+require '../item'
+
 class Genre 
-    
+    attr_accessor :name
+    attr_reader :id, :items
+
+    def initialize(name)
+        @name = name
+        @id = rand (1...1000)
+        @items = []
+    end
+
+    def add_item(item)
+        @items << item
+        item.add_genre self
+    end
 end
