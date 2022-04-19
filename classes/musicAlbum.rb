b0887b42c4c6084dd10d27fefef16c01be306df2
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-19 15:34:38 +0545

    Implement can_be_archived method

--- classes/musicAlbum.rb
+++ classes/musicAlbum.rb
@@ -1,6 +1,17 @@
 require_relative '../item'
 require 'data'
 
-class MusicAlbum
-    
+class MusicAlbum < Item
+    attr_accessor :on_spotify, :name, :publish_date
+    attr_reader :items
+    def initialize(on_spotify, name, publish_date)
+        super(name, publish_date,)
+        @id = id
+        @on_spotify = on_spotify
+        @name = name
+        @items = []
+    end
+    def can_be_archived?
+        super && @on_spotify
+    end
 end
