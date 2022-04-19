19e0d5bda1be705626304a9122c6da3926ba8539
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-19 18:37:51 +0545

    Fix linter file

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,17 +1,17 @@
 require '../item'
 
-class Genre 
-    attr_accessor :name
-    attr_reader :id, :items
+class Genre
+  attr_accessor :name
+  attr_reader :id, :items
 
-    def initialize(name)
-        @name = name
-        @id = rand (1...1000)
-        @items = []
-    end
+  def initialize(name)
+    @name = name
+    @id = rand(1...1000)
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.add_genre self
-    end
+  def add_item(item)
+    @items << item
+    item.add_genre self
+  end
 end
---
+++ classes/music_album.rb
@@ -0,0 +1,18 @@
+require '../item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify, :name, :publish_date
+  attr_reader :items
+
+  def initialize(on_spotify, name, publish_date)
+    super(publish_date)
+    @id = id
+    @on_spotify = on_spotify
+    @name = name
+    @items = []
+  end
+
+  def can_be_archived?
+    super && @on_spotify
+  end
+end
