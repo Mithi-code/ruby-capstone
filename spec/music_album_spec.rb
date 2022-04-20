0e37d3ec5eff017bdfb00a049636318ca176c824
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-20 19:15:37 +0545

    Implement the function for testing

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,16 +1,19 @@
 require_relative '../classes/music_album'
+require 'date'
 
 describe MusicAlbum do
   context 'It should create a musicAlbum instance with onspotify, name and published date' do
-    music = MusicAlbum.new(publish_date: '2012-05-01', name: true, on_spotify: 'Hello')
+    music = MusicAlbum.new(on_spotify: false, name: 'hello', publish_date: DateTime.parse('2012-05-01'))
     it 'creates new musicAlbum with the given parameters' do
       expect(music).to be_an_instance_of MusicAlbum
     end
     it 'should check if it can be archived' do
-      expect(music.on_spotify).to eq 'Hello'
+      music.move_to_archive
+      is_archived = music.archived
+      expect(is_archived).to eq false
     end
     it 'Should check the musicAlbum name' do
-      expect(music.name).to eq true
+      expect(music.name).to eq 'hello'
     end
   end
 end
