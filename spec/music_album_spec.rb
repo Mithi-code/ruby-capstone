be264b6fd2a4bb3335dba606763ff3210bbce6df
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-20 14:58:30 +0545

    Add test for the music_album

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../classes/music_album'
+
+describe MusicAlbum do
+  context 'It should create a musicAlbum instance with onspotify, name and published date' do
+    music = MusicAlbum.new(publish_date:'2012-05-01', name:true, on_spotify:'Hello')
+    it 'creates new musicAlbum with the given parameters' do
+      expect(music).to be_an_instance_of MusicAlbum
+    end
+    it 'should check if it can be archived' do
+      expect(music.on_spotify).to eq 'Hello'
+    end
+    it 'Should check the musicAlbum name' do
+      expect(music.name).to eq true
+    end
+  end
+end
