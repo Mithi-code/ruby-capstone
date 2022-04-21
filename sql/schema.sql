5a52303ff56677c58975bcf957ba4917bfa20897
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-04-21 14:32:22 +0530

    added label and book table to database

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,12 +4,15 @@ CREATE TABLE author(
         first_name TEXT,
         last_name TEXT
 );
-
 CREATE TABLE source(
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         'name' TEXT
 );
-
+CREATE TABLE label(
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        title TEXT,
+        color TEXT
+);
 CREATE TABLE game(
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         multiplayer BOOLEAN,
@@ -29,6 +32,21 @@ CREATE TABLE game(
 CREATE TABLE movie(
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         silent BOOLEAN,
+        publish_date DATE,
+        archived BOOLEAN,
+        genre_id INTEGER,
+        source_id INTEGER,
+        author_id INTEGER,
+        label_id INTEGER,
+        FOREIGN KEY(genre_id) REFERENCES genre(id),
+        FOREIGN KEY(source_id) REFERENCES source(id),
+        FOREIGN KEY(author_id) REFERENCES author(id),
+        FOREIGN KEY(label_id) REFERENCES label(id)
+);
+CREATE TABLE book(
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        cover_state TEXT,
+        publisher TEXT,
         publish_date DATE,
         archived BOOLEAN,
         genre_id INTEGER,
