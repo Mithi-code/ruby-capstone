8c6d1c0e5a42bfaa1a7118f978bed5241d022aba
rachidelaid <rachidelaid@gmail.com>
2022-04-21 08:03:43 +0000

    adding the movie and source table

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,10 +4,31 @@ CREATE TABLE author(
         first_name TEXT,
         last_name TEXT
 );
+
+CREATE TABLE source(
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        'name' TEXT
+);
+
 CREATE TABLE game(
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         multiplayer BOOLEAN,
         last_played_at DATE,
+        publish_date DATE,
+        archived BOOLEAN,
+        genre_id INTEGER,
+        source_id INTEGER,
+        author_id INTEGER,
+        label_id INTEGER,
+        FOREIGN KEY(genre_id) REFERENCES genre(id),
+        FOREIGN KEY(source_id) REFERENCES source(id),
+        FOREIGN KEY(author_id) REFERENCES author(id),
+        FOREIGN KEY(label_id) REFERENCES label(id)
+);
+
+CREATE TABLE movie(
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        silent BOOLEAN,
         publish_date DATE,
         archived BOOLEAN,
         genre_id INTEGER,
